81ab5d147d1b96a041bcd521ca2349fe08604803
unknown <vic.valesca@gmail.com>
2024-09-24 19:33:05 -0300

    ingresi imag

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |i|
+    Image.create(url_image: Faker::Avatar.image)
+   end
